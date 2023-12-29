4da7c2ce4c0dde02dd163f945f011518708040d8
akiJacky <tyatya38an@gmail.com>
2023-12-29 15:59:04 +0900

    init project

---
+++ FlashFunction/FlashFunction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashFunction
+//
+//  Created by Akihiro Nakano on 2023/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
