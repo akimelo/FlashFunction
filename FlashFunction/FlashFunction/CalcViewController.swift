0b0437b58ef161d67b5ee688750128e53d617e9d
akiJacky <tyatya38an@gmail.com>
2023-12-29 19:30:51 +0900

    modified

---
+++ FlashFunction/FlashFunction/CalcViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalcViewController.swift
+//  FlashFunction
+//
+//  Created by Akihiro Nakano on 2023/12/29.
+//
+
+import UIKit
+
+class CalcViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
